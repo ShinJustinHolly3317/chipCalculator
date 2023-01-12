a82d96709b106e0107e03768e6f2a50440cbd890
Justin Kao <justin.kao@choco.media>
2023-01-12 20:54:35 +0800

    add pm2 ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+    name   : "app1",
+    script : "./app.js",
+    env: {
+      SQL_DATABASE: "database-1.cyx5mqn9dxib.us-east-1.rds.amazonaws.com",
+    }
+  }]
+}
